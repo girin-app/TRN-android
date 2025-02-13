1aa7ccac3eafe73bbc60a6b0d75e7d030ca60198
Uther <uther@girin.app>
2025-02-14 04:25:25 +0900

    fix: remove unused code and bump up versions

--- trn/build.gradle.kts
+++ trn/build.gradle.kts
@@ -1,47 +1,8 @@
 plugins {
-//    id("com.android.library")
-//    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.jvm")
     `maven-publish`
 }
 
-//android {
-//    namespace = "app.girin.trn_android"
-//    compileSdk = 35
-//
-//    defaultConfig {
-//        minSdk = 24
-//
-//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//        consumerProguardFiles("consumer-rules.pro")
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//            proguardFiles(
-//                getDefaultProguardFile("proguard-android-optimize.txt"),
-//                "proguard-rules.pro"
-//            )
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_1_8
-//        targetCompatibility = JavaVersion.VERSION_1_8
-//    }
-//    kotlinOptions {
-//        jvmTarget = "1.8"
-//    }
-//}
-//java {
-//    sourceCompatibility = JavaVersion.VERSION_1_8
-//    targetCompatibility = JavaVersion.VERSION_1_8
-//}
-//kotlin {
-//    compilerOptions {
-//        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
-//    }
-//}
 dependencies {
     // Define a BOM and its version
     api(platform(libs.ethers.bom))
@@ -61,7 +22,7 @@ publishing {
             from(components["java"])
             groupId = "com.github.girin-app"
             artifactId = "TRN-android"
-            version = "v0.3.0"
+            version = "v0.3.1"
 
             pom {
                 name.set("TRN-android")
